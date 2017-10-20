97bec7c1e33854c3bd3f136b5bc908949c2b1c25
Harshil Patel <h.gibbs62@gmail.com>
2017-10-20 15:29:07 -0500

    session fix

--- apps/loginReg/views.py
+++ apps/loginReg/views.py
@@ -6,42 +6,42 @@ from django.contrib.messages import error
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~ Renders ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #=================================================================
 
-def index(req):
-    return render(req, 'loginReg/index.html')
+def index(request):
+    return render(request, 'loginReg/index.html')
 
-def home(req):
-    if 'user_id' not in req.session:
+def home(request):
+    if 'user_id' not in request.session:
         return redirect('/')
 
-    print req.session['user_id']
+    print request.session['user_id']
     
-    return render(req, 'loginReg/home.html')
+    return render(request, 'loginReg/home.html')
 
 #=================================================================
 #~~~~~~~~~~~~~~~~~~~~~~~~~~ Processes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #=================================================================
 
-def login(req):
-    user = User.objects.login(req.POST)
+def login(request):
+    user = User.objects.login(request.POST)
     if user:
-        req.session['user_id'] = user.id
-        print req.session['user_id']
+        request.session['user_id'] = user.id
+        print request.session['user_id']
         return redirect('/home')
     
-    error(req, 'Invalid email or password')
+    error(request, 'Invalid email or password')
     return redirect('/')
 
-def register(req):
-    errors = User.objects.validate(req.POST)
+def register(request):
+    errors = User.objects.validate(request.POST)
     if len(errors):
         for key, message in errors.iteritems():
-            error(req, message, extra_tags=key)
+            error(request, message, extra_tags=key)
         return redirect('/')
     else:
-        user = User.objects.createUser(req.POST)
-        req.session['user_id'] = user.id
+        user = User.objects.createUser(request.POST)
+        request.session['user_id'] = user.id
         return redirect('/home')
     
-def logoff(req):
-    req.session.clear()
+def logoff(request):
+    request.session.clear()
     return redirect('/')
